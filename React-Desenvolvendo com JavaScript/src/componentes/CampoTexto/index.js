4521574e197661c8927a34b572890d9d2261d639
Paula Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2023-08-21 10:55:00 -0300

    Update components
    FEAT: Enhance Form and Add Save Event
    
    feat: Improve the Formulario component with required fields and a Save event.
    
    In this commit, the Formulario component has been upgraded to enhance user experience and data validation:
    - Added a Save event handler (`aoSalvar`) to the form, preventing its default behavior and showing an alert upon submission.
    - Implemented required field indication for certain input fields using the `obrigatorio` prop.
    - Imported and integrated the newly created Button component (`Botao`) to the form.
    
    These improvements ensure better usability and data validation for creating a collaborator's card.

--- React-Desenvolvendo com JavaScript/src/componentes/CampoTexto/index.js
+++ React-Desenvolvendo com JavaScript/src/componentes/CampoTexto/index.js
@@ -7,7 +7,7 @@ const CampoTexto = (props) => {
             <label>
                 {props.label}
             </label>
-            <input placeholder={props.placeholder}/>
+            <input required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
--- React-Desenvolvendo com JavaScript/src/componentes/ListaSuspensa/index.js
+++ React-Desenvolvendo com JavaScript/src/componentes/ListaSuspensa/index.js
@@ -3,8 +3,11 @@ import './ListaSuspensa.css'
 const ListaSuspensa = (props) => {
     return (
         <div className='lista-suspensa'>
-            <label>{props.label}</label>
-            <select>
+            <label>
+                {props.label}
+            </label>
+
+            <select required={props.required}>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
